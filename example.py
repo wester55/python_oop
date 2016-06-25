47bd01a6125f5b80af1867dd7e604039549b3745
Sasha Segal <wester.55@gmail.com>
2016-06-25 13:30:14 +0300

    fix to example.py

--- example.py
+++ example.py
@@ -1,15 +1,15 @@
 from random import randint
 from time import strftime
+from traceback import print_exc
 
 class Compute(object):
-    count = 0
 
-    def __init__(self):
-        self.count += 1
-        self.object_type = 'I am a ' + self.__class__.__name__
+    @property
+    def object_type(self):
+        print '%s' % ('I am a ' + self.__class__.__name__)
 
     def ps(self):
-        print "There are a lot of processes running"
+        print '%s' % ("There are a lot of processes running")
 
     def echo(self,echoinput):
         print echoinput
@@ -18,45 +18,70 @@ class Compute(object):
         return randint(1,100)
 
 class Container(Compute):
+
+    instance_count = 0
+
+    @property
+    def status(self):
+        print '%s' % (self._status)
+
     def __init__(self):
+        Container.instance_count += 1
         self._status = "stopped"
 
     def start(self):
         self._status = "started"
-        print "Container Started"
+        print '%s' % ("Container Started")
 
     def stop(self):
         self._status = "stopped"
-        print "Container Stopped"
-
-    @property
-    def status(self):
-        return self._status
+        print '%s' % ("Container Stopped")
 
 class VM(Compute):
+
+    instance_count = 0
+
+    def __init__(self):
+        VM.instance_count += 1
+
     def snapshot(self):
         self._snapshot_date = strftime("%H:%M:%S")
-        print "snapshot taken"
+        print '%s' % ("snapshot taken")
 
     @property
     def snapshot_date(self):
         return self._snapshot_date
 
-class Physical(Compute):
+    def __getattribute__(self, name):
+        print '%s' % (super(VM, self).__getattribute__(name))
+
+class EInvalidBootObject(Exception):
     def __init__(self):
-        self.bios_boot_priority = "hdd"
+        message = "valid values: usb, cd, hdd"
+        super(EInvalidBootObject, self).__init__(message)
+
+class Physical(Compute):
+
+    instance_count = 0
+
+    def __init__(self):
+        Physical.instance_count += 1
+        self.bios_boot_priority = 'hdd'
 
     def __setattr__(self, name, value):
-        if name == 'bios_boot_priority':
-            if value not in ['usb', 'cd', 'hdd']:
-                raise AttributeError("%s can't be set to %s" %(name, value))
-                exit(1)
-        super(Physical, self).__setattr__(name, value)
+        try:
+            if name == 'bios_boot_priority':
+                if value not in ['usb', 'cd', 'hdd']:
+                    raise EInvalidBootObject
+            super(Physical, self).__setattr__(name, value)
+        except EInvalidBootObject:
+            print_exc()
 
-    def __getattr__(self, name):
+    def __getattribute__(self, name):
         if name in ['virt_ip']:
             raise AttributeError('no such method')
-        return super(Physical, self).__getattribute__(name)
+        else:
+            print '%s' % (super(Physical, self).__getattribute__(name))
 
 
 
